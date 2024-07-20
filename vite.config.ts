95da6ff5dda0cbeb74fd3a054986730002188970
Dominic Price <dominicprice@outlook.com>
2024-07-20 10:56:34 +0100

    fix define issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,23 @@
 import fs from "fs";
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    define: {
-        APP_VERSION: JSON.stringify(process.env.npm_package_version),
-        LICENCE: JSON.stringify(
-            fs.readFileSync("./LICENSE", { encoding: "utf-8" }),
-        ),
-    },
-    resolve: {
-        alias: {
-            "#": "/src",
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+
+    return {
+        plugins: [react()],
+        define: {
+            APP_VERSION: JSON.stringify(env.npm_package_version),
+            LICENCE: JSON.stringify(
+                fs.readFileSync("./LICENSE", { encoding: "utf-8" }),
+            ),
         },
-    },
+        resolve: {
+            alias: {
+                "#": "/src",
+            },
+        },
+    };
 });
